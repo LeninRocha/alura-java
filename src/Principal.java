7eca7632676c6df65804c0a0210a516c733e05ac
Lênin Rocha de Araujo Pinheiro <lenin@myeshop.com.br>
2023-08-29 21:52:31 -0300

    primeir push

---
+++ src/Principal.java
@@ -0,0 +1,27 @@
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.pegaMedia());
+        System.out.println(meuFilme.getTotalDeAvaliacoes());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        lost.exibeFichaTecnica();
+        System.out.println("Duração da serie: " + lost.getDuracaoEmMinutos());
+    }
+}
